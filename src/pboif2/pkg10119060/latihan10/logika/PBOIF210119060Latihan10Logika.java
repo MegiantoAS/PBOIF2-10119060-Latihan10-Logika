83e5702f5ae9ee70e1b600418b8f71af3800d305
asus <asus@LAPTOP-BD2P4KVT>
2020-10-15 20:04:00 +0700

    new commit

---
+++ src/pboif2/pkg10119060/latihan10/logika/PBOIF210119060Latihan10Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119060.latihan10.logika;
+
+/**
+ *
+ * @author Megianto Adi saputra
+ * Nama         : Megianto Adi Saputra
+ * Kelas        : IF-2
+ * NIM          : 10119060
+ * Deskripsi    : Latihan Logika
+ */
+public class PBOIF210119060Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            int umur = 19;
+            boolean dewasa;
+            dewasa = (umur >= 17);
+            System.out.println("Status dewasa :" + dewasa);
+        }
+    }
+    
+
